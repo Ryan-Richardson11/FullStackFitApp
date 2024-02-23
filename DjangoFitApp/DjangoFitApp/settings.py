a30db66b1db786965fd4b922ec3b7d8300536696
Ryan Richardson <ryanrich811@gmail.com>
2024-02-23 11:02:59 -0500

    reconfigured react and django; template code showing up on local server

--- DjangoFitApp/DjangoFitApp/settings.py
+++ DjangoFitApp/DjangoFitApp/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FitnessData',
         'USER': 'root',
-        'PASSWORD': 'Michigan21!',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
